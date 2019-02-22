a4efb93ac836d77c67c05604753e1f3d767a2381
李佩儒 <@CATHAY-O8DD1VTQ>
2019-02-22 15:34:24 +0800

    init

---
+++ design-pattern/src/main/java/sharing1_without_factory/CalculatorWioutFactory.java
@@ -0,0 +1,43 @@
+package sharing1_without_factory;
+
+/**
+ * 只留下業務邏輯的部分<br>
+ * 優點是開發迅速<br>
+ * 缺點是如果要新增計算方法就要進來修改計算邏輯的程式，不符合開放-關閉的開發模式<br>
+ * 
+ * @author Frieda
+ *
+ */
+public class CalculatorWioutFactory {
+	private static int result;
+
+	/**
+	 * 計算方法，輸入數字a,b，運算方法operator，進行相對應的計算
+	 * 
+	 * @param a
+	 * @param b
+	 * @param operate
+	 * @return
+	 * @throws Exception
+	 */
+	public int calculate(int a, int b, String operator) throws Exception {
+		switch (operator) {
+		case "+":
+			result = a + b;
+			break;
+		case "-":
+			result = a - b;
+			break;
+		case "*":
+			result = a * b;
+			break;
+		case "/":
+			result = a / b;
+			break;
+		default:
+			throw new Exception("輸入錯誤");
+		}
+		return result;
+	}
+
+}
