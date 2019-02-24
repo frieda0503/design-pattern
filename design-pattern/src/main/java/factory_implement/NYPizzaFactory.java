7513588f061277353b27318a3546a746243988a0
frieda <frieda@192.168.0.6>
2019-02-24 23:43:12 +0800

    update

--- design-pattern/src/main/java/factory_implement/NYPizzaFactory.java
+++ design-pattern/src/main/java/factory_implement/NYPizzaFactory.java
@@ -7,4 +7,17 @@ package factory_implement;
  */
 public class NYPizzaFactory extends SimplePizzaFactory {
 	
+	@Override
+	public Pizza createPizza(String type) {
+		Pizza pizza = null;
+		if (type.equals("cheese")) {
+			pizza = new NYCheesePizza();
+		} else if (type.equals("tomato")) {
+			pizza = new TomatoSaucePizza();
+		} else if (type.equals("veggie")) {
+			pizza = new VeggiePizza();
+		}
+
+		return pizza;
+	}
 }
